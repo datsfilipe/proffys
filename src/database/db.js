071c2266b84d91b76c10c8c7f036bdbbc1c9d7f2
datsfilipe <datsfilipe@gmail.com>
2021-08-04 18:59:50 -0300

    just looking around, ignore this commit

--- src/database/db.js
+++ src/database/db.js
@@ -29,4 +29,4 @@ function execute(db){
     `)
 }
 
-module.exports = Database.open(__dirname + '/database.sqlite').then(execute)
+module.exports = Database.open(__dirname + '/database.sqlite').then(execute);
